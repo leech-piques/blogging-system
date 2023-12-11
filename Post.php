583603b4f01ab0ed03586d565a3ec8a65adfbee1
Grace Thompson <153483632+leech-piques@users.noreply.github.com>
2023-12-11 12:59:47 +0700

    Create Post.php

---
+++ Post.php
@@ -0,0 +1,28 @@
+<!-- Post.php -->
+
+<?php
+
+class Post {
+    private $title;
+    private $content;
+    private $author;
+
+    public function __construct($title, $content, $author) {
+        $this->title = $title;
+        $this->content = $content;
+        $this->author = $author;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getContent() {
+        return $this->content;
+    }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+}
+?>
